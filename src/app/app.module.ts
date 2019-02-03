ee24adcb492e75129c20a74b9501683f6d31c8c7
den3538 <den3538@gmail.com>
2019-02-03 13:47:14 +0200

    ngModel init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListsModule, routs} from './lists';
 import { InterpolationComponent } from './interpolation/interpolation.component';
 import { AttributeBindingsComponent } from './attribute-bindings/attribute-bindings.component';
+import { BidirectionalComponent } from './bidirectional/bidirectional.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InterpolationComponent,
     AttributeBindingsComponent,
+    BidirectionalComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ListsModule,
+    FormsModule,
     RouterModule.forRoot(routs)
   ],
   providers: [],
--- src/app/lists/list.routs.ts
+++ src/app/lists/list.routs.ts
@@ -2,6 +2,7 @@
 import {HelloWorldListComponent, List2ComponentComponent, CounterHostComponent, OutputHostComponent} from './index';
 import {InterpolationComponent} from '../interpolation/interpolation.component';
 import {AttributeBindingsComponent} from '../attribute-bindings/attribute-bindings.component';
+import {BidirectionalComponent} from '../bidirectional/bidirectional.component';
 
 
 export const routs = [
@@ -11,6 +12,7 @@ export const routs = [
   {path: 'list4', component: OutputHostComponent},
   {path: 'interpolation', component: InterpolationComponent},
   {path: 'attrBind', component: AttributeBindingsComponent},
+  {path: 'bidirectional', component: BidirectionalComponent},
   {path: '', redirectTo: 'hello2', pathMatch: 'full'}
 ]
 ;
